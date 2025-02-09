40e571b2eb1d1a13d8cc94e4aa871e5bf7d03e6c
Gideon Onita <onitagideon30@gmail.com>
2025-02-09 11:04:17 +0100

    hover effect on each skill modified

--- src/components/EachSkill.jsx
+++ src/components/EachSkill.jsx
@@ -1,20 +1,12 @@
 const EachSkill = ({ logo, title, level }) => {
   return (
     <li className="">
-      {/* <div className="flex justify-between text-lg">
-        {type} <span className="text-base-content">{level}</span>
-      </div>
-      <progress
-        className="progress progress-secondary w-full bg-base-300/75"
-        value={progress}
-        max="100"
-      /> */}
-      <div className="bg-base-300 pt-5 pb-3 w-[170px] h-[130px] rounded-2xl border border-base-300 hover:border hover:border-primary hover:bg-primary/30 skill-card transition transition-300">
+      <div className="bg-base-300 pt-5 pb-3 w-[170px] h-[130px] rounded-2xl border border-base-300 hover:border hover:border-primary hover:bg-primary/5 skill-card transition transition-300">
         <figure className=" card-image w-[60px] h-[60px] mx-auto">
           <img
             src={logo}
             alt={title}
-            className="w-full h-full logo grayscale"
+            className="w-full h-full logo grayscale z-40"
           />
         </figure>
         <p className="text-center font-bold text-lg mt-2 text-primary grayscale card-level">
