691283486b0105ccd6932972d819704463e7e180
Gideon Onita <onitagideon30@gmail.com>
2025-08-15 00:35:17 +0100

    fix:fix image resize bug

--- src/components/global/Avatar.jsx
+++ src/components/global/Avatar.jsx
@@ -6,21 +6,13 @@ const Avatar = ({ img, isLoading }) => {
           <p className="text-3xl font-bold gradient-text"> G.O </p>
         </figure>
       ) : (
-        <figure className="w-30 h-30 md:w-36 md:h-36 rounded-full border-4 border-primary/20">
+        <figure className="w-34 h-34 rounded-full border-4 border-primary/20">
           <img
-            src={`${img}?w=112&h=112&fit=thumb`}
-            srcSet={`
-      ${img}?w=112&h=112&fit=thumb 1x,
-      ${img}?w=224&h=224&fit=thumb 2x,
-      ${img}?w=144&h=144&fit=thumb 768w,
-      ${img}?w=288&h=288&fit=thumb 768w 2x
-    `}
-            sizes="(max-width: 767px) 112px, 144px"
+            src={`${img}?w=136&h=136&fit=thumb`}
             alt="Gideon Onita"
-            width={112}
-            height={112}
+            width={136}
+            height={136}
             className="rounded-full aspect-square object-cover"
-            loading="lazy"
             decoding="async"
           />
         </figure>
