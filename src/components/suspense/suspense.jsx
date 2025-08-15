1b433f84c002aecd284268da4554d0bf29e1cc86
Gideon Onita <onitagideon30@gmail.com>
2025-08-15 05:34:49 +0100

    fix:remove hero section from above fold

--- src/components/suspense/suspense.jsx
+++ src/components/suspense/suspense.jsx
@@ -3,7 +3,26 @@ import LazyLoad from 'react-lazyload'
 
 export const sectionSuspense = (Component) => (
   <LazyLoad>
-    <Suspense fallback={<div className="container py-20" />}>
+    <Suspense
+      fallback={
+        <div className="container h-[50vh] flex items-center justify-center">
+          <span className="loading loading-spin loading-sm "></span>
+        </div>
+      }
+    >
+      {Component}
+    </Suspense>
+  </LazyLoad>
+)
+export const heroSectionSuspense = (Component) => (
+  <LazyLoad>
+    <Suspense
+      fallback={
+        <div className="container h-[100vh] flex items-center justify-center">
+          <span className="loading loading-spin loading-sm "></span>
+        </div>
+      }
+    >
       {Component}
     </Suspense>
   </LazyLoad>
