7e40906b4061f056df62a506ace518a3e5cffbec
Gideon Onita <onitagideon30@gmail.com>
2025-08-15 05:44:46 +0100

    fix:import heroSectionSuspense

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,19 +1,22 @@
 import Header from '../components/header/Header'
-import Hero from '../components/hero/Hero'
-import { sectionSuspense } from '../components/suspense/suspense'
+import {
+  heroSectionSuspense,
+  sectionSuspense,
+} from '../components/suspense/suspense'
 import { lazy } from 'react'
 const About = lazy(() => import('../components/about/About'))
 const Skills = lazy(() => import('../components/skills/Skills'))
 const Projects = lazy(() => import('../components/projects/Projects'))
 const Contact = lazy(() => import('../components/contact/Contact'))
 const Footer = lazy(() => import('../components/footer/Footer'))
+const Hero = lazy(() => import('../components/hero/Hero'))
 
 const Index = () => {
   return (
     <>
       <Header />
       <main id="hero container">
-        <Hero />
+        {heroSectionSuspense(<Hero />)}
         {sectionSuspense(<About />)}
         {sectionSuspense(<Skills />)}
         {sectionSuspense(<Projects />)}
