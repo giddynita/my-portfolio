18155b922cb3c7df3e4e3671f73e3071db807117
Gideon Onita <onitagideon30@gmail.com>
2025-08-15 01:42:25 +0100

    fix: modify error page buttons

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,17 @@
 import { Link, useRouteError } from 'react-router-dom'
 import pageNotFound from '../assets/images/page_not_found.svg'
+import GradientButton from '../components/global/GradientButton'
+import OutlineButton from '../components/global/OutlineButton'
 
 const Error = () => {
   const error = useRouteError()
-  console.log(error.message)
   if (error.status === 404) {
     return (
       <div className="h-screen flex flex-col gap-y-8 items-center justify-center">
         <img src={pageNotFound} alt="pageNotFound" className=" w-1/3" />
         <h3 className="text-xl font-bold">This page can't be found</h3>
-        <Link to="/" className="text-secondary text-lg font-medium">
-          Return to home
+        <Link to="/" className="text-secondary text-lg font-medium w-48">
+          <GradientButton text="Return to home" type="button" />
         </Link>
       </div>
     )
@@ -18,6 +19,9 @@ const Error = () => {
   return (
     <div className="h-screen flex flex-col gap-y-8 items-center justify-center">
       <h3 className="text-xl font-bold">Something went wrong</h3>
+      <a href={window.location.href} className="w-48">
+        <OutlineButton text="Reload page" type="button" />
+      </a>
     </div>
   )
 }
